1d39ab727e57f654db6f0c57910f2a4f39831f13
Ben Coppock <ben@goodpixelmedia.com>
2025-05-11 01:07:22 -0400

    Add formatter

---
+++ .formatter.exs
@@ -0,0 +1,13 @@
+[
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    holding_register: 2,
+    holding_register: 3,
+    input_register: 2,
+    input_register: 3,
+    coil: 2,
+    coil: 3,
+    input: 2,
+    input: 3
+  ]
+]
