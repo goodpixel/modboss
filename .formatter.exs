5302ce79c48466300438edd0151767d48a3de312
Ben Coppock <ben@goodpixelmedia.com>
2025-05-29 02:32:39 -0700

    Export formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,13 +1,18 @@
+locals_without_parens = [
+  holding_register: 2,
+  holding_register: 3,
+  input_register: 2,
+  input_register: 3,
+  coil: 2,
+  coil: 3,
+  discrete_input: 2,
+  discrete_input: 3
+]
+
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    holding_register: 2,
-    holding_register: 3,
-    input_register: 2,
-    input_register: 3,
-    coil: 2,
-    coil: 3,
-    discrete_input: 2,
-    discrete_input: 3
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
   ]
 ]
